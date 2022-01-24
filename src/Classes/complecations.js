37f911ee57554c7747fd3af2113099a0cf185c09
hharutyunyan111@gmail.com <hharutyunyan111@gmail.com>
2022-01-24 07:29:16 +0400

    change level

--- src/Classes/complecations.js
+++ src/Classes/complecations.js
@@ -4,348 +4,348 @@ export const complications =  [
         level: [1, 2],
         flaskCount:2,
         ballCount:2,
-        time: 30
+        time: 60
     },
     {
         level: [2, 3],
         flaskCount:3,
         ballCount:2,
-        time: 30
+        time: 120
     },
     {
         level: [3, 4],
         flaskCount:3,
         ballCount:3,
-        time: 40,
+        time: 210,
         addRow: true
     },
     {
         level: [4, 5],
         flaskCount:3,
         ballCount:4,
-        time: 70,
+        time: 320,
         addRow: true
     },
     {
         level: [5, 6],
         flaskCount:3,
         ballCount:5,
-        time: 80,
+        time: 400,
         addRow: true
     },
     {
         level: [6, 7],
         flaskCount:3,
         ballCount:6,
-        time: 100,
+        time: 450,
         addRow: true
     },
     {
         level: [7, 8],
         flaskCount:4,
         ballCount:2,
-        time: 30,
+        time: 120,
         addRow: true
     },
     {
         level: [8, 9],
         flaskCount:4,
         ballCount:3,
-        time: 80,
+        time: 200,
         addRow: true
     },
     {
         level: [9, 10],
         flaskCount:4,
         ballCount:4,
-        time: 90,
+        time: 300,
         addRow: true
     },
     {
         level: [10, 11],
         flaskCount:4,
         ballCount:5,
-        time: 100,
+        time: 350,
         addRow: true
     },
     {
         level: [11, 12],
         flaskCount:4,
         ballCount:6,
-        time: 125,
+        time: 400,
         addRow: true
     },
     {
         level: [12, 13],
         flaskCount:4,
         ballCount:7,
-        time: 150,
+        time: 500,
         addRow: true
     },
     {
         level: [13, 14],
         flaskCount:4,
         ballCount:8,
-        time: 200,
+        time: 550,
         addRow: true
     },
     {
         level: [14, 15],
         flaskCount:5,
         ballCount:2,
-        time: 40,
+        time: 120,
         addRow: true
     },
     {
         level: [15, 16],
         flaskCount:5,
         ballCount:3,
-        time: 60,
+        time: 250,
         addRow: true
     },
     {
         level: [16, 17],
         flaskCount:5,
         ballCount:4,
-        time: 80,
+        time: 350,
         addRow: true
     },
     {
         level: [17, 18],
         flaskCount:6,
         ballCount:2,
-        time: 50,
+        time: 130,
         addRow: true
     },
     {
         level: [18, 19],
         flaskCount:6,
         ballCount:3,
-        time: 80,
+        time: 250,
         addRow: true
     },
     {
         level: [19, 20],
         flaskCount:6,
         ballCount:4,
-        time: 120,
+        time: 300,
         addRow: true
     },
     {
         level: [20, 21],
         flaskCount:6,
         ballCount:5,
-        time: 180,
+        time: 360,
         addRow: true
     },
     {
         level: [21, 22],
         flaskCount:7,
         ballCount:2,
-        time: 80,
+        time: 150,
         addRow: true
     },
     {
         level: [22, 23],
         flaskCount:7,
         ballCount:3,
-        time: 150,
+        time: 250,
         addRow: true
     },
     {
         level: [23, 24],
         flaskCount:7,
         ballCount:4,
-        time: 200,
+        time: 350,
         addRow: true
     },
     {
         level: [24, 25],
         flaskCount:7,
         ballCount:5,
-        time: 250,
+        time: 400,
         addRow: true
     },
     {
         level: [25, 26],
         flaskCount:8,
         ballCount:2,
-        time: 60,
+        time: 140,
         addRow: true
     },
     {
         level: [26, 27],
         flaskCount:8,
         ballCount:3,
-        time: 90,
+        time: 300,
         addRow: true
     },
     {
         level: [27, 28],
         flaskCount:8,
         ballCount:4,
-        time: 120,
+        time: 400,
         addRow: true
     },
     {
         level: [28, 29],
         flaskCount:8,
         ballCount:5,
-        time: 160,
+        time: 500,
         addRow: true
     },
     {
         level: [29, 30],
         flaskCount:3,
         ballCount:3,
-        time: 120,
+        time: 150,
         addRow: true
     },
     {
         level: [30, 31],
         flaskCount:3,
         ballCount:4,
-        time: 250,
+        time: 350,
         addRow: true
     },
     {
         level: [31, 32],
         flaskCount:3,
         ballCount:5,
-        time: 300,
+        time: 400,
         addRow: true
     },
     {
         level: [32, 33],
         flaskCount:3,
         ballCount:6,
-        time: 400,
+        time: 500,
         addRow: true
     },
     {
         level: [33, 34],
         flaskCount:4,
         ballCount:2,
-        time: 120,
+        time: 160,
         addRow: true
     },
     {
         level: [34, 35],
         flaskCount:4,
         ballCount:3,
-        time: 160,
+        time: 250,
         addRow: true
     },
     {
         level: [35, 37],
         flaskCount:4,
         ballCount:4,
-        time: 250,
+        time: 350,
         addRow: true
     },
     {
         level: [37, 39],
         flaskCount:4,
         ballCount:5,
-        time: 300,
+        time: 500,
         addRow: true
     },
     {
         level: [39, 41],
         flaskCount:4,
         ballCount:6,
-        time: 350,
+        time: 600,
         addRow: true
     },
     {
         level: [41, 43],
         flaskCount:4,
         ballCount:7,
-        time: 450,
+        time: 650,
         addRow: true
     },
     {
         level: [43, 45],
         flaskCount:4,
         ballCount:8,
-        time: 600,
+        time: 750,
         addRow: true
     },
     {
         level: [45, 47],
         flaskCount:5,
         ballCount:3,
-        time: 150,
+        time: 300,
         addRow: true
     },
     {
         level: [47, 49],
         flaskCount:5,
         ballCount:4,
-        time: 250,
+        time: 350,
         addRow: true
     },
     {
         level: [49, 51],
         flaskCount:5,
         ballCount:5,
-        time: 350,
+        time: 450,
         addRow: true
     },
     {
         level: [51, 53],
         flaskCount:6,
         ballCount:2,
-        time: 150,
+        time: 200,
         addRow: true
     },
     {
         level: [53, 55],
         flaskCount:6,
         ballCount:3,
-        time: 250,
+        time: 300,
         addRow: true
     },
     {
         level: [55, 57],
         flaskCount:6,
         ballCount:4,
-        time: 350,
+        time: 450,
         addRow: true
     },
     {
         level: [57, 59],
         flaskCount:6,
         ballCount:5,
-        time: 500,
+        time: 550,
         addRow: true
     },
     {
         level: [59, 61],
         flaskCount:7,
         ballCount:2,
-        time: 160,
+        time: 200,
         addRow: true
     },
     {
         level: [61, 63],
         flaskCount:7,
         ballCount:3,
-        time: 260,
+        time: 400,
         addRow: true
     },
     {
         level: [63, 65],
         flaskCount:7,
         ballCount:4,
-        time: 390,
+        time: 450,
         addRow: true
     },
     {
         level: [65, 67],
         flaskCount:7,
         ballCount:5,
-        time: 550,
+        time: 600,
         addRow: true
     },
     {
